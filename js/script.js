cedcc8cdec921291cad8794412d29227f9175096
AlessandroBonifazi <alessandrobonifazi21@gmail.com>
2022-03-09 16:32:17 +0100

    adding js mail array and loop with dom

--- js/script.js
+++ js/script.js
@@ -1,2 +1,18 @@
 // Debug
 console.log('JS OK!')
+
+// Mail
+const email = document.getElementById('email');
+
+// Array
+const emailList = ['email1@mail.com', 'email2@mail.com', 'email3@mail.com', 'email4@mail.com'];
+
+login.addEventListener('click', function () {
+    for (let i = 0; i < emailList.length; i++) {
+        if (email.value === emailList[i]) {
+            console.log('Welcome back!');
+        } else {
+            console.log('Wrong email!');
+        }
+    }
+})
